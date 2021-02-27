8da6b358273aa25ad91538e8229d8c230eaca674
DonsWayo <djwayomix@gmail.com>
2021-02-27 14:36:20 +0100

    Initial Commit

---
+++ Joke Book/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Joke Book
+//
+//  Created by DonsWayo on 27/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Joke Book/Joke_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Joke_BookApp.swift
+//  Joke Book
+//
+//  Created by DonsWayo on 27/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Joke_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
